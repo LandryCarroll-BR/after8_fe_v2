62dff311635728d15fe48c7e328493e8504d2f37
landtry <landry@boldist.co>
2023-05-29 04:25:59 -0500

    created contact form

---
+++ components/contact-client.tsx
@@ -0,0 +1,134 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/react-hook-form/form"
+
+const FormSchema = z.object({
+  firstName: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .regex(new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)),
+  emailConfrim: z
+    .string()
+    .regex(new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/)),
+})
+
+export function InputReactHookForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      emailConfrim: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    alert(JSON.stringify(data))
+  }
+
+  return (
+    <div className="container">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto w-2/3 space-y-6"
+        >
+          <div className="flex items-center justify-between gap-4">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="first name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="last name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="email address" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="emailConfrim"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email Confrimation</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="email address confirmation"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
---
+++ components/contact.tsx
@@ -0,0 +1,94 @@
+import { fetchAPI } from "@/lib/api"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { Textarea } from "./ui/textarea"
+
+const SUBMIT_FORM = `
+  mutation SubmitForm($databaseId: ID!, $fieldValues: [FormFieldValuesInput]!) {
+    submitGfForm(input: { id: $databaseId, fieldValues: $fieldValues }) {
+      confirmation {
+        message
+      }
+      errors {
+        id
+        message
+      }
+    }
+  }
+`
+
+export function ContactForm() {
+  async function onSubmit(formData: FormData) {
+    "use server"
+    const first = formData.get("first-name")
+    const last = formData.get("last-name")
+    const email = formData.get("email")
+    const message = formData.get("message")
+    fetchAPI(SUBMIT_FORM, {
+      variables: {
+        databaseId: 1,
+        fieldValues: [
+          {
+            id: 1,
+            nameValues: {
+              first,
+              last,
+            },
+          },
+          {
+            id: 2,
+            emailValues: {
+              value: email,
+            },
+          },
+          {
+            id: 4,
+            value: message,
+          },
+        ],
+      },
+    })
+  }
+
+  return (
+    <div className="container my-16 font-sans">
+      <h1 className="mb-8 text-center  text-5xl font-bold">Contact Us</h1>
+      <p className="mb-6 text-center">
+        Interested in having us at your event? Drop us a line!
+      </p>
+      <form action={onSubmit} className="mx-auto space-y-6 lg:max-w-2xl">
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex-1">
+            <Label>First Name</Label>
+            <Input required name="first-name" placeholder="Taylor" />
+          </div>
+
+          <div className="flex-1">
+            <Label>Last Name</Label>
+            <Input required name="last-name" placeholder="Swift" />
+          </div>
+        </div>
+
+        <div>
+          <Label>Email</Label>
+          <Input
+            required
+            name="email"
+            type="email"
+            placeholder="address@email.com"
+          />
+        </div>
+
+        <div>
+          <Label>Message</Label>
+          <Textarea name="message" placeholder="Enter your message here." />
+        </div>
+        <Button className={"font-bold uppercase"} type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  )
+}
