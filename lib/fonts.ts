895632409f2b689752ebafc13456b618a0875672
landtry <landry@boldist.co>
2023-05-29 04:27:25 -0500

    removed unused font imports

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import { JetBrains_Mono as FontMono } from "next/font/google"
 import localFont from "next/font/local"
 
 export const fontSans = localFont({
